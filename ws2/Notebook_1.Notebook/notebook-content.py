dc1b8857f1729fe1eab05f95033e4f9567f2b671
Yaron Pri-Gal <89332819+yaronpri@users.noreply.github.com>
2026-01-11 18:51:14 +0200

    Committing 5 items from workspace 1bcea411-8217-4224-a2be-bfff1f9038da

---
+++ ws2/Notebook_1.Notebook/notebook-content.py
@@ -0,0 +1,36 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+from notebookutils.variableLibrary import getLibrary
+variable_library_object = getLibrary("DemoVL")
+variable_value = variable_library_object.notebookVariable
+print(f"The value of YourVariableName is: {variable_value}")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
