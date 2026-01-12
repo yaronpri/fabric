89a0e9065994ac473f22b83967d734ba7dfe5284
Yaron Pri-Gal <89332819+yaronpri@users.noreply.github.com>
2026-01-12 10:03:12 +0200

    Committing 1 item from workspace 1bcea411-8217-4224-a2be-bfff1f9038da

--- ws2/UserDataFunction_1.UserDataFunction/function_app.py
+++ ws2/UserDataFunction_1.UserDataFunction/function_app.py
@@ -4,10 +4,13 @@ import logging
 
 udf = fn.UserDataFunctions()
 
+@udf.connection(argName="varLib", alias="demoVL")
 @udf.function()
-def hello_fabric(name: str) -> str:
+def hello_fabric(name: str, varLib: fn.FabricVariablesClient) -> str:
     logging.info('Python UDF trigger function processed a request.')
 
-    return f"Welcome to Fabric Functions, {name}, at {datetime.datetime.now()}!"
+    variables = varLib.getVariables()
+    notebookV = variables.get("notebookVariable")
+    return f"Welcome to Fabric Functions, {name}, value of var= {notebookV} at {datetime.datetime.now()}!"
 
 
