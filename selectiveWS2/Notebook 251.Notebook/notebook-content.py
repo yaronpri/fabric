b39f26efccbe8e4d98e424322f6d366dbc2654bb
Yaron Pri-Gal <89332819+yaronpri@users.noreply.github.com>
2026-02-15 11:58:28 +0200

    Committing 1 item from workspace 55bb0da6-b31e-4b7f-9b7d-27f85aa4d086

--- selectiveWS2/Notebook 251.Notebook/notebook-content.py
+++ selectiveWS2/Notebook 251.Notebook/notebook-content.py
@@ -1,1 +1,23 @@
-# No content
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+print(1)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
